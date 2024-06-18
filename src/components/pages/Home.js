39ce69737bb7c2a83c7ed902b44feb464ad0780c
aksinghania <akshtsinghnia@gmail.com>
2024-06-19 00:24:10 +0530

    Added vision elements in homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -29,6 +29,27 @@ function Home() {
             </div>
           </div>
         </section>
+
+        <section className="section">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 mb-5 text-center">
+                <h3 className="main-heading">Vision, Mission and Values</h3>
+                <div className="underline mx-auto"></div>
+              </div>
+              <div className="col-md-4">
+                <h6>Our Vision</h6>
+                <p>
+                  We aim to inspire innovation, foster collaboration, and
+                  nurture technical skills in a supportive and inclusive
+                  environment. We strive to empower members to become
+                  forward-thinking tech leaders who make meaningful
+                  contributions to the world.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
