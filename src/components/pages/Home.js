8ca73bd91f6d9135b182f6f40ad0da520231a3f4
aksinghania <akshtsinghnia@gmail.com>
2024-06-19 00:41:38 +0530

    added mission and values

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../inc/Navbar";
 import Slider from "../inc/Slider";
 import { Link } from "react-router-dom";
+import VMC from "./inc/Vmc";
 
 function Home() {
   return (
@@ -30,26 +31,7 @@ function Home() {
           </div>
         </section>
 
-        <section className="section">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 mb-5 text-center">
-                <h3 className="main-heading">Vision, Mission and Values</h3>
-                <div className="underline mx-auto"></div>
-              </div>
-              <div className="col-md-4">
-                <h6>Our Vision</h6>
-                <p>
-                  We aim to inspire innovation, foster collaboration, and
-                  nurture technical skills in a supportive and inclusive
-                  environment. We strive to empower members to become
-                  forward-thinking tech leaders who make meaningful
-                  contributions to the world.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
+        <VMC />
       </div>
     </>
   );
---
+++ src/components/pages/inc/Vmc.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function VMC() {
+  return (
+    <section className="section bg-c-light border-top">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 mb-4 text-center">
+            <h3 className="main-heading">Vision, Mission and Values</h3>
+            <div className="underline mx-auto"></div>
+          </div>
+          <div className="col-md-4 text-center">
+            <h6>Our Vision</h6>
+            <p>
+              We aim to inspire innovation, foster collaboration, and nurture
+              technical skills in a supportive and inclusive environment. We
+              strive to empower members to become forward-thinking tech leaders
+              who make meaningful contributions to the world.
+            </p>
+          </div>
+
+          <div className="col-md-4 text-center">
+            <h6>Our Mission</h6>
+            <p>
+              Our mission is to provide a supportive platform for students to
+              explore and excel in coding, foster innovation through
+              collaborative projects, and build an inclusive community that
+              equips members with the skills for success in the tech industry.
+            </p>
+          </div>
+
+          <div className="col-md-4 text-center">
+            <h6>Our Core Values</h6>
+            <p>
+              We value innovation, collaboration, and inclusivity. We uphold
+              excellence, integrity, and a commitment to lifelong learning,
+              fostering a supportive environment where members can thrive and
+              innovate in technology.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default VMC;
